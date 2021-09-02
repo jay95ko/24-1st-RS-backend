c7945598cb3bacfb761a842643e223a976f2608d
jayko <jayko95@naver.com>
2021-09-02 18:46:14 +0900

    Product API with Restful Add: urls.py, views.py 상품개별, 상품리스트, 삼품디테일 페이지에서 필요한 요소 각각 endpoint 구현

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import ProductListView
+from .views import ProductView, DetailView, ImageListView, SidedishListView, BreweryView, FlavorListView, ProductListView
 
 urlpatterns = [
-    path("", ProductListView.as_view()),
+    path("/<int:product_id>", ProductView.as_view()),
+    path("/<int:product_id>/detail", DetailView.as_view()),
+    path("/<int:product_id>/images", ImageListView.as_view()),
+    path("/<int:product_id>/sidedishes", SidedishListView.as_view()),
+    path("/<int:product_id>/flavors", FlavorListView.as_view()),
+    path("/<int:product_id>/brewery", BreweryView.as_view()),
+    path("/list", ProductListView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 import random
-from typing import Counter
+from django.core.exceptions import FieldError
 
 from django.http import JsonResponse
 from django.views import View
 from django.db.models import Q
 
-from .models import Category, Product
+from .models import Category, Product, ProductFlavor
 
 def MakingList(queryset):
     list = []
@@ -22,86 +22,153 @@ def MakingList(queryset):
                 "id"               : product.id,
                 "name"             : product.name,
                 "price"            : product.price,
+                "dgree"            : product.dgree,
+                "ml"               : product.ml,
+                "awards"           : product.awards,
                 "tiny_description" : product.tiny_description,
                 "hash"             : hash_tag,
                 "grade"            : product.grade,
                 "image"            : first_image_url,
+                "expire_date"      : product.expire_date,
+                "keep"             : product.keep,
+                "category_name"    : product.category.name,
             }
         )
     return list
+
+class ImageListView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+
+            images_queryset = product.images.all()
+            images          = []
+            for image in images_queryset:
+                images.append(image.image_url)
+            return JsonResponse({"Result": images}, status=200)
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
+class BreweryView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+
+            breweries = []
+            breweries.append(
+                {
+                    "brewery_name" : product.brewery.name,
+                    "brewery_image": product.brewery.img_url,
+                    }
+                )
+            return JsonResponse({"Result": breweries}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
+class FlavorListView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+
+            products_flavors_queryset = ProductFlavor.objects.filter(product=product)
+            flavors                   = []
+            for product_flavor in products_flavors_queryset:
+                flavors.append(
+                    {
+                        "flavor_name"      : product_flavor.flavor.flavor_name,
+                        "point"            : product_flavor.point,
+                    }
+                )
+            return JsonResponse({"Result": flavors}, status=200)
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
+class SidedishListView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+
+            sidedishs_queryset = product.sidedish.all()
+            sidedishs          = []
+            for sidedish in sidedishs_queryset:
+                sidedishs.append(
+                    {
+                        "name"      : sidedish.name,
+                        "image_url" : sidedish.image_url,
+                    }
+                )
+            return JsonResponse({"Result": sidedishs}, status=200)
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
+class DetailView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+
+            detailes = []
+            detailes.append(
+                {
+                "flavor" : product.descriptions.get().point_flavor,
+                "side"   : product.descriptions.get().point_side,
+                "story"  : product.descriptions.get().point_story,
+                    }
+                )
+            return JsonResponse({"Result": detailes}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+class ProductView(View):
+    def get(self, request, product_id):
+        try:
+            product      = Product.objects.get(id=product_id)
+            product_info = MakingList(product)
+            return JsonResponse({"Result": product_info}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
 class ProductListView(View):
     def get(self, request):
+        try:
+            ORDER_BY = request.GET.get("order_by", "id")
+            #created_at, grade, price
+            OFFSET   = int(request.GET.get("offset", 0))
+            LIMIT    = int(request.GET.get("limit", 2))
+            CATEGORY = request.GET.get("category")
+            RANDOM   = request.GET.get("random", False)
 
-        #rice_wine(약주, 청주)는 두가지의 주종을 포함하고 있음으로 Q객체 사용
-        # fruit_wine = Product.objects.filter(category_id = 1)
-        # spirits    = Product.objects.filter(category_id = 2)
-        # takju      = Product.objects.filter(category_id = 4)
-        # rice_wine  = Product.objects.filter(Q(category_id = 5) | Q(category_id = 6))
+            products = Product.objects.all().order_by(ORDER_BY)
 
-        # category_product_list = [fruit_wine, spirits, takju, rice_wine]
-        
-        # random_number_list = []
-        # for num in range(0,2):
-        #     random_number_list.append(random.randint(0,9))
-        
-        # main_product_list      = []
-        # recommend_product_list = []
-        # sale_product_list      = []
-        # popular_product_list   = []
-        # new_product_list       = []
-        # recommend_objects      = []
+            if CATEGORY:
+                category = Category.objects.get(name = CATEGORY)
+                products = products.filter(category = category)
 
-        # for product_id in random_number_list:
-        #     for category in category_product_list:
-        #         recommend_objects.append(category[product_id])
-        # recommend_product_list = MakingList(recommend_objects)
-        
-        # sale_product         = Product.objects.get(id=15)
-        # sale_product_img_url = sale_product.images.all()[:1].get().image_url
-        # sale_product_list    = [
-        #     {
-        #         "id"               : sale_product.id,
-        #         "name"             : sale_product.name,
-        #         "price"            : sale_product.price,
-        #         "image"            : sale_product_img_url,
-        #     }
-        # ]
-        
-        # popular_products     = Product.objects.all().order_by('-grade')[:3]
-        # popular_product_list = MakingList(popular_products)
-        
-        # new_products     = Product.objects.all().order_by('-created_at')[:3]
-        # new_product_list = MakingList(new_products)
+            if RANDOM:
+                products_queryset  = []
+                random_number_list = []
 
-        # main_product_list.append(
-        #     {
-        #         "recommend_product_list" : recommend_product_list,
-        #         "sale_product_list"      : sale_product_list,
-        #         "popular_product_list"   : popular_product_list,
-        #         "new_product_list"       : new_product_list,
-        #     }
-        # )
+                for num in range(0,LIMIT):
+                    random_number = random.randint(0,products.count()-1)
 
-        # return JsonResponse({"Result": main_product_list}, status=200)
+                    while random_number in random_number_list:
+                        random_number = random.randint(0,products.count()-1)
 
+                    random_number_list.append(random_number)
+                    products_queryset.append(products[random_number])
 
-        ORDER_BY = request.GET.get("order_by", id)
-        #created_at, grade, price
-        COUNT    = int(request.GET.get("count", 2))
-        CATEGORY = request.GET.get("category")
-        RANDOM   = request.GET.get("random", False)
+            else:
+                products_queryset = products[OFFSET:OFFSET+LIMIT]
 
-        category = Category.objects.get(name = CATEGORY)
-        products = Product.objects.filter(category = category).order_by("-" + ORDER_BY)
+            products_info = MakingList(products_queryset)
+            return JsonResponse({"Result": products_info}, status=200)
 
-        if RANDOM:
-            products_queryset = []
-            for num in range(0,COUNT):
-                products_queryset.append(products[random.randint(0,products.count()-1)])
+        except FieldError:
+            return JsonResponse({"Result": "ORDER_BY_ERROR"}, status=400)
 
-        else:
-            products_queryset = products[:COUNT]
+        except Category.DoesNotExist:
+            return JsonResponse({"Result": "CATEGORY_DOES_NOT_EXIST"}, status=400)
 
-            
-
-
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
