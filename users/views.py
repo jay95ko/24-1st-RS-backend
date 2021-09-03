bea904e8dfb2f29d08efb15ed689ac7c3f2313b2
fall031 <fall900802@gmail.com>
2021-09-03 19:22:50 +0900

    ADD: users.views.py(login DB요청  중복제거)

--- users/views.py
+++ users/views.py
@@ -48,18 +48,21 @@ class SignupView(View):
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
 
-class LoginView(View):
-    def post(self, request):
+class LoginView(View):   
+      def post(self, request):
         try:
             data = json.loads(request.body)
 
             if not (data['email'] and data['password']):
                 return JsonResponse({"MESSAGE":"EMPTY_VALUE_ERROR"}, status=400)
 
-            if User.objects.filter(email=data['email']).exists():
-                if bcrypt.checkpw(data['password'].encode('utf-8'), User.objects.get(email=data['email']).password.encode('utf-8')):
-                    access_token = jwt.encode({'id':User.objects.get(email=data['email']).id}, SECRET_KEY, algorithm=ALGORITHM)
-                    return JsonResponse({"MESSAGE":"SUCCESS","ACCESS_TOKEN":access_token}, status=201)
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({"MESSAGE":"INVALID_USER"}, status=401)
+
+            user = User.objects.get(email=data['email'])
+            if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                access_token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm=ALGORITHM)
+                return JsonResponse({"MESSAGE":"SUCCESS","ACCESS_TOKEN":access_token}, status=201)
             
             return JsonResponse({"MESSAGE":"INVALID_USER"}, status=401)
 
