e345fc2f62163e00fbf6f0303b9ef9918011c0be
fall031 <fall900802@gmail.com>
2021-09-05 10:11:40 +0900

    ADD: carts basic

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from carts.models import Cart
+from users.models import User
+from products.models import Product
+
+class CartView(View):
+    # 데코레이터 필요
+    def post(self, request):
+        try:
+            data       = json.loads(request.body)
+            product_id = data['product_id']
+            quantity   = data['quantity']
+            if not Product.objects.get(id=product_id):
+                return JsonResponse({"MESSAGE":"DOES_NOT_EXIST_ERROR"}, status=400)
+
+            Cart.objects.create(
+                user_id = 3             ,# 데코레이터에서 가져오기(토큰)
+                product_id = product_id,
+                quantity = quantity,
+                )
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+    # def put
--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from users.views import SignupView
 
 urlpatterns = [
-    path('users', include('users.urls'))
+    path('users', include('users.urls')),
+    path('carts', include('carts.urls')),
 ]
