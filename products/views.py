260d91d3cc38a42875afecf461a44ad14ef5cb34
jayko <jayko95@naver.com>
2021-09-06 10:49:37 +0900

    ProductListView, ProductView 상품의 리스트를 받아올 수 있는 endpoint 구현 상품 하나의 정보만 받아오는 endpoint 구현(추후에 디테일 정보를 가져달 때 같이 활용)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.core.exceptions import FieldError
+from django.http import JsonResponse
+from django.views import View
+from django.db.models import Q
+
+from .models import Category, Product
+
+def MakingList(queryset):
+    products = queryset.select_related("category", "brewery").prefetch_related("images", "tag", "sidedish")
+    result   = [{
+                "id"               : product.id,
+                "name"             : product.name,
+                "price"            : format(product.price, ","),
+                "dgree"            : product.dgree,
+                "ml"               : product.ml,
+                "awards"           : product.awards,
+                "tiny_description" : product.tiny_description,
+                "hash"             : [{"caption" : tag.caption} for tag in product.tag.all()],
+                "grade"            : product.grade,
+                "image"            : product.images.all()[0].image_url,
+                "expire_date"      : product.expire_date,
+                "keep"             : product.keep,
+                "category_name"    : product.category.name,
+                "side_dish"        : [{"name" : sidedish.name, "image_url" : sidedish.image_url} for sidedish in product.sidedish.all()],
+            } for product in products]
+    return result
+
+class ProductView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.filter(id=product_id)
+            result  = MakingList(product)
+            return JsonResponse({"Result": result}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
+class ProductListView(View):
+    def get(self, request):
+        try:
+            ORDER_BY   = request.GET.get("order-by", "id")
+            OFFSET     = int(request.GET.get("offset", 0))
+            LIMIT      = int(request.GET.get("limit", 2))
+            CATEGORIES = request.GET.get("category")
+            RANDOM     = bool(request.GET.get("random"))
+            MIN_PRICE  = request.GET.get("min-price", 0)
+            MAX_PRICE  = request.GET.get("max-price", 1000000)
+            DEGREES    = request.GET.get("degree")
+            products   = Product.objects.all().order_by(ORDER_BY)
+
+            if CATEGORIES:
+                query = Q()
+                for CATEGORY in CATEGORIES.split(","):
+                    query |= Q(category__name = CATEGORY)
+                products = products.filter(query)
+
+            if RANDOM:
+                products_queryset  = []
+                random_number_list = []
+                for num in range(0,LIMIT):
+                    random_number = random.randint(0,products.count()-1)
+
+                    while random_number in random_number_list:
+                        random_number = random.randint(0,products.count()-1)
+
+                    random_number_list.append(random_number)
+                    products_queryset.append(products[random_number])
+                result = MakingList(products_queryset)
+                return JsonResponse({"Result": result}, status=200)
+
+            if DEGREES:
+                query = Q()
+                for DEGREE in DEGREES.split(","):
+                    print(DEGREE)
+                    query |= (Q(dgree__gte = int(DEGREE)-10) & Q(dgree__lte = int(DEGREE)))
+                products = products.filter(query)
+
+            query = Q(price__gte = MIN_PRICE) & Q(price__lte = MAX_PRICE)
+            products = products.filter(query)
+
+            products_queryset = products[OFFSET:OFFSET+LIMIT]
+
+            result = MakingList(products_queryset)
+            return JsonResponse({"Result": result}, status=200)
+
+        except FieldError:
+            return JsonResponse({"Result": "ORDER_BY_ERROR"}, status=400)
+
+        except Category.DoesNotExist:
+            return JsonResponse({"Result": "CATEGORY_DOES_NOT_EXIST"}, status=400)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"Result": "PRODUCT_DOES_NOT_EXIST"}, status=400)
