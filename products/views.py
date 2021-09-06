0f0a8dbc909946ba42d0a8b7a6e18a0fe8cffaba
jayko <jayko95@naver.com>
2021-09-06 11:03:07 +0900

    CategoryView 카테고리별 이미지url, 설명, 이름을 전달하는 endpoint

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models import Category
+
+class CategoryView(View):
+    def get(self, request, category):
+        try:
+            if category.startswith("약주") or category.endswith("청주"):
+                category = "약주"
+            category = Category.objects.get(name__istartswith=category)
+
+            if category.name == "약주" or category.name == "청주":
+                result = {
+                    "name"        : "약주,청주",
+                    "description" : category.description,
+                    "image_url"   : category.image_url,
+                }
+                return JsonResponse({"Result": result}, status=200)
+            
+            result = {
+                    "name"        : category.name,
+                    "description" : category.description,
+                    "image_url"   : category.image_url,
+                }
+            return JsonResponse({"Result": result}, status=200)
+
+        except Category.DoesNotExist:
+            return JsonResponse({"Result": "CATEGORY_DOES_NOT_EXIST"}, status=400)
