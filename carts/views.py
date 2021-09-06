36b3bfad19fc8f16504cc51ea1a4d0f5f5c550e9
fall031 <fall900802@gmail.com>
2021-09-06 10:38:50 +0900

    ADD: carts basic(2)

--- carts/views.py
+++ carts/views.py
@@ -8,23 +8,59 @@ from users.models import User
 from products.models import Product
 
 class CartView(View):
-    # 데코레이터 필요
+    #@데코레이터
     def post(self, request):
-        try:
             data       = json.loads(request.body)
             product_id = data['product_id']
             quantity   = data['quantity']
-            if not Product.objects.get(id=product_id):
-                return JsonResponse({"MESSAGE":"DOES_NOT_EXIST_ERROR"}, status=400)
+           
+            try:
+                if not Product.objects.filter(id=product_id).exists():
+                    return JsonResponse({"MESSAGE":"DOES_NOT_EXIST_ERROR"}, status=400)
 
-            Cart.objects.create(
-                user_id = 3             ,# 데코레이터에서 가져오기(토큰)
-                product_id = product_id,
-                quantity = quantity,
-                )
-            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+                Cart.objects.create(
+                    user_id = 3             ,# 데코레이터에서 가져오기(토큰유저정보)
+                    product_id = product_id,
+                    quantity = quantity,
+                    )
+                return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
-        except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+            except KeyError:
+                return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
 
-    # def put
+    # def put(self, request):
+    #         data = json.loads(request.body)
+    #         count = data['count']
+    #         delete = data['delete']
+    #         cart = Cart.objects.get(id=id)
+        
+    #         try:
+    #             if count == '+' and cart.quantity > 0:
+    #                 cart.quantity += 1
+
+    #             if count == '-' and cart.quantity > 1:
+    #                 cart.quantity -= 1
+
+    #             if delete:
+    #                 cart.delete()
+    #                 return JsonResponse({'MESSAGE':'DELETE'}, status=201)
+
+    #             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+    #         except KeyError:
+    #             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+    #@데코레이터
+    def get(self, request):
+        # carts = Cart.objects.filter(user_id=3).values()
+        print('aa')
+        result = list(Cart.objects.filter(user_id=3).values('user_id','product_id','quantity'))
+        # for cart in carts:
+        #     result.append(
+        #         {
+        #             'user': cart.user_id,
+        #             'product': cart.product_id,
+        #             'quantity': cart.quantity,
+        #         }
+        #     )
+        return JsonResponse({'RESULT':result}, status=200)
