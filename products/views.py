cb615d9114e83c7a831baed39266436409ddcc1a
jayko <jayko95@naver.com>
2021-09-07 18:12:47 +0900

    Modify: change view logic

--- products/views.py
+++ products/views.py
@@ -4,27 +4,29 @@ from django.views import View
 from .models import Category
 
 class CategoryView(View):
-    def get(self, request, category):
+    def get(self, request, category_id):
         try:
-            if category.startswith("약주") or category.endswith("청주"):
-                category = "약주"
-                category = Category.objects.get(name__istartswith=category)
-
-                result = {
-                    "id"          : "5,6",
-                    "name"        : "약주,청주",
-                    "description" : category.description,
-                    "image_url"   : category.image_url,
-                }
-                return JsonResponse({"Result": result}, status=200)
-            
-            category = Category.objects.get(name__istartswith=category)
+            category = Category.objects.get(id=category_id)
             result = {
-                    "id"          : category.id,
                     "name"        : category.name,
                     "description" : category.description,
                     "image_url"   : category.image_url,
                 }
+            return JsonResponse({"Result": result}, status=200)
+
+        except Category.DoesNotExist:
+            return JsonResponse({"Result": "CATEGORY_DOES_NOT_EXIST"}, status=400)
+
+class CategoryListView(View):
+    def get(self, request):
+        try:
+            categories = Category.objects.all()
+            result = [{
+                    "id"          : category.id,
+                    "name"        : category.name,
+                    "description" : category.description,
+                    "image_url"   : category.image_url,
+                } for category in categories]
             return JsonResponse({"Result": result}, status=200)
 
         except Category.DoesNotExist:
